9869c3b1957caf1bbf2930ea22ef9d330fce27df
Popin Marius <marius.popin@insa-cvl.fr>
2020-12-13 22:37:44 +0100

    Cleanup

--- bot.js
+++ bot.js
@@ -1,6 +1,5 @@
 require('dotenv').config()
 
-
 const Discord = require('discord.js')
 const client = new Discord.Client({
     partials:["MESSAGE"]
@@ -13,38 +12,38 @@ client.on('ready', () => {
 })
 
 client.on('message',msg => {
-    if (msg.content === `${PREFIX}${pdrole}`){
+    if (msg.content === `${pdrole}`){
         msg.member.roles.add("779842251328192592")
         msg.reply('Bienvenue dans le rôle de pd')
         //msg.channel.send('Bienvenue dans le rôle de pd')
-    
-
     }
-}) 
+    
+    if (msg.content === `"Pd"`){
+        msg.member.roles.add("779842251328192592")
+        msg.reply('Bienvenue dans le rôle de pd')
+        //msg.channel.send('Bienvenue dans le rôle de pd')
+    }
 
-client.on('message',msg => {
     if (msg.content === "Je t'aime"){
         msg.channel.send('Moi aussi, de tout mon coeur')
         msg.react("❤️")
     }
-}) 
 
-client.on('message',msg => {
+    if (msg.content === "Merci cousin"){
+        msg.channel.send('Tout le plaisir est pour moi')
+        msg.react("🥰")
+    }
+
     if (msg.content === "Bon app"){
         msg.channel.send('Merci toi aussi')
         msg.react("🍴")
     }
-}) 
 
-client.on('message',msg => {
     if (msg.content === "Donne moi un A"){
         msg.channel.send('Je suis un meilleur bot')
         msg.react("🅰️")
     }
-}) 
 
-
-client.on('message',msg => {
     if (msg.content === `${"Je suis "}${"Alex"}`){
         if (msg.content === 'Alex'){
             msg.channel.send('Salut le Boug')
@@ -57,7 +56,7 @@ client.on('message',msg => {
 }) 
 
 client.on('messageDelete',msg => {
-    msg.reply("Gros pd")
+    //msg.reply("Gros pd")
     //msg.channel.send("Un message a été supprimé, la police est là pour s'en charger")
 })
 
