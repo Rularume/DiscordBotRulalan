deb8fca5d4ab6fcd13c673bd3cc94a9a465e4253
Popin Marius <marius.popin@insa-cvl.fr>
2020-12-14 15:51:01 +0100

    Noel feature

--- bot.js
+++ bot.js
@@ -4,29 +4,65 @@ const Discord = require('discord.js')
 const client = new Discord.Client({
     partials:["MESSAGE"]
 })
-const PREFIX = '!'
+PREFIX = '!'
 const pdrole = "pd"
+const Noel="noel"
+const noel="noël"
 
 client.on('ready', () => {
   console.log(`Our bot is ready to go!!!!`)
 })
 
+
+
 client.on('message',msg => {
-    if (msg.content === `${pdrole}`){
+    if (or(msg.content === `${pdrole}`,msg.content === `"Pd"`)){
+        
+        msg.member.roles.remove("787746606266843196")
         msg.member.roles.add("779842251328192592")
+        msg.react("🇵")
+        msg.react("🇩")
         msg.reply('Bienvenue dans le rôle de pd')
         //msg.channel.send('Bienvenue dans le rôle de pd')
+        
+        
     }
+    if (or(msg.content == `${PREFIX}${Noel}`,msg.content == `${PREFIX}${noel}`)){
+        msg.channel.send("joyeux noel")
+        //msg.send("test")
+        msg.react("🎄")
+        msg.react("🎅")
+    }
+
+    if (msg.content == `${PREFIX}${"test25+645413"}`){
+        //msg.reply("test sucessfull")
+        PREFIX = "-"
+        msg.reply(PREFIX)
+    }
+
+    if (msg.content == `${PREFIX}${"prefix"}`){
+        msg.channel.send("test sucessfull")
+        PREFIX = "-"
+        msg.reply(PREFIX)
+    }
+
     
-    if (msg.content === `"Pd"`){
-        msg.member.roles.add("779842251328192592")
-        msg.reply('Bienvenue dans le rôle de pd')
-        //msg.channel.send('Bienvenue dans le rôle de pd')
+
+    if (msg.content == "Jchuis pas pd"){
+        msg.member.roles.remove("779842251328192592")
+        msg.react("🏳️‍🌈")
+        msg.reply("Ben super t'es homophobe ou quoi ??")
+        msg.member.roles.add("787746606266843196")
+        msg.channel.send("Tu as reçu le rôle d'homophobe")
     }
 
     if (msg.content === "Je t'aime"){
         msg.channel.send('Moi aussi, de tout mon coeur')
         msg.react("❤️")
+    }
+
+    if (msg.content === "123"){
+        msg.channel.send('pd')
     }
 
     if (msg.content === "Merci cousin"){
@@ -48,21 +84,31 @@ client.on('message',msg => {
         msg.react("🅰️")
     }
 
-    if (msg.content === `${"Je suis "}${"Alex"}`){
-        if (msg.content === 'Alex'){
-            msg.channel.send('Salut le Boug')
-        }
-        if (msg.content === 'Marius'){
-            msg.channel.send('Salut le bg')
-        }
-        msg.channel.send('Default')
-    }
+    
 }) 
 
 client.on('messageDelete',msg => {
+    if (msg.content == "pd"){
+        msg.channel.send("Ya un homophobe dans la salle qui supprime les pd")
+    }
     //msg.reply("Gros pd")
     //msg.channel.send("Un message a été supprimé, la police est là pour s'en charger")
 })
 
 
 client.login(process.env.BOT_TOKEN)
+
+function and(a, b) {
+    if (a) {
+      if (b) {
+        return 1;
+      }
+    }
+    return 0;
+  }
+
+function or(a, b) {
+    if (a) { return 1; }
+    if (b) { return 1; }
+    return 0;
+  }
